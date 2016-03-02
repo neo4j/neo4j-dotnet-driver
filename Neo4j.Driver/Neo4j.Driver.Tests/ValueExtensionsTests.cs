551150dbbcff9759c1a10d5bc3005a1597b39c35
Zhen Li <zhen.li@neotechnology.com>
2016-03-02 13:11:24 +0100

    Added a extension method `As<T>` to explicitly cast a object to T Usage example: ``` 	foreach (var record in cursor.Stream())         {             INode node = record["movie"];             movies.Add(new Movie{               released = node.Properties["released"].As<int>(),               tagline = node.Properties["tagline"].As<string>(),               title = node.Properties["title"].As<string>(),               actors = node.Properties["actors"].AsList(x=>x.As<string>()) });         } ```

---
+++ Neo4j.Driver/Neo4j.Driver.Tests/ValueExtensionsTests.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Neo4j.Driver.Internal;
+using Xunit;
+
+namespace Neo4j.Driver.Tests
+{
+    public class ValueExtensionsTests
+    {
+        public class AsPrimaryTypeMethod
+        {
+
+            [Fact]
+            public void ShouldConvertNullToNullable()
+            {
+                object value = null;
+                var actual = value.As<bool?>();
+                actual.HasValue.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldConvertNullToString()
+            {
+                object value = null;
+                var actual = value.As<string>();
+                actual.Should().BeNull();
+            }
+
+            [Fact]
+            public void ShouldConvertNullToClass()
+            {
+                object value = null;
+                var actual = value.As<INode>();
+                actual.Should().BeNull();
+            }
+
+            [Fact]
+            public void ShouldThrowExceptionWhenCovertingFromNullToNotNullable()
+            {
+                object value = null;
+                var ex = Record.Exception(() => value.As<bool>());
+                ex.Should().BeOfType<NotSupportedException>();
+                ex.Message.Should().Be("Unsupported cast from `null` to `System.Boolean`");
+            }
+
+            [Theory]
+            [InlineData(0, false)]
+            [InlineData(1234L, true)]
+            [InlineData(1.2f, true)]
+            [InlineData(1.2, true)]
+            [InlineData((short)1, true)]
+            [InlineData((sbyte)1, true)]
+            [InlineData((ulong)1234, true)]
+            [InlineData((uint)1234, true)]
+            [InlineData((ushort)1, true)]
+            [InlineData((byte)1, true)]
+            [InlineData(true, true)]
+            [InlineData(false, false)]
+            public void ShouldConvertToNullable(object input, bool expected)
+            {
+                var actual = input.As<bool?>();
+                actual.HasValue.Should().BeTrue();
+                actual.Value.Should().Be(expected);
+            }
+
+            [Theory]
+            [InlineData(0, false)]
+            [InlineData(1234L, true)]
+            [InlineData(1.2f, true)]
+            [InlineData(1.2, true)]
+            [InlineData((short)1, true)]
+            [InlineData((sbyte)1, true)]
+            [InlineData((ulong)1234, true)]
+            [InlineData((uint)1234, true)]
+            [InlineData((ushort)1, true)]
+            [InlineData((byte)1, true)]
+            [InlineData(true, true)]
+            [InlineData(false, false)]
+            public void ShouldConvertToBool(object input, bool expected)
+            {
+                var actual = input.As<bool>();
+                actual.Should().Be(expected);
+            }
+
+            [Theory]
+            [InlineData(1234, 1234)]
+            [InlineData(1234L, 1234)]
+            [InlineData(1.2f, 1)]
+            [InlineData(1.2, 1)]
+            [InlineData((short)1, 1)]
+            [InlineData((sbyte)1, 1)]
+            [InlineData((ulong)1234, 1234)]
+            [InlineData((uint)1234, 1234)]
+            [InlineData((ushort)1, 1)]
+            [InlineData((byte)1, 1)]
+            [InlineData("1", 1)]
+            [InlineData('a', 97)]
+            [InlineData(true, 1)]
+            public void ShouldConvertToInt(object input, int expected)
+            {
+                var actual = input.As<int>();
+                actual.Should().Be(expected);
+            }
+
+            [Theory]
+            [InlineData(1234, 1234)]
+            [InlineData(1234L, 1234)]
+            [InlineData(1.2f, 1)]
+            [InlineData(1.2, 1)]
+            [InlineData((short)1, 1)]
+            [InlineData((sbyte)1, 1)]
+            [InlineData((ulong)1234, 1234)]
+            [InlineData((uint)1234, 1234)]
+            [InlineData((ushort)1, 1)]
+            [InlineData((byte)1, 1)]
+            [InlineData("1", 1)]
+            [InlineData('a', 97)]
+            [InlineData(true, 1)]
+            public void ShouldConvertToLong(object input, long expected)
+            {
+                var actual = input.As<long>();
+                actual.Should().Be(expected);
+            }
+
+            [Theory]
+            [InlineData(1234, 1234)]
+            [InlineData(1234L, 1234)]
+            [InlineData(1.2f, 1.2)]
+            [InlineData(1.2, 1.2)]
+            [InlineData((short)1, 1)]
+            [InlineData((sbyte)1, 1)]
+            [InlineData((ulong)1234, 1234)]
+            [InlineData((uint)1234, 1234)]
+            [InlineData((ushort)1, 1)]
+            [InlineData((byte)1, 1)]
+            [InlineData("1", 1)]
+            [InlineData(true, 1)]
+            public void ShouldConvertToDouble(object input, double expected)
+            {
+                var actual = input.As<double>();
+                (actual - expected).Should().BeLessThan(0.01);
+            }
+
+            [Theory]
+            [InlineData(12, 12)]
+            [InlineData(12L, 12)]
+            [InlineData(1.2f, 1)]
+            [InlineData(1.2, 1)]
+            [InlineData((short)1, 1)]
+            [InlineData((sbyte)1, 1)]
+            [InlineData((ulong)12, 12)]
+            [InlineData((uint)12, 12)]
+            [InlineData((ushort)1, 1)]
+            [InlineData((byte)1, 1)]
+            [InlineData("1", 1)]
+            [InlineData('a', 97)]
+            [InlineData(true, 1)]
+            public void ShouldConvertToByte(object input, byte expected)
+            {
+                var actual = input.As<byte>();
+                actual.Should().Be(expected);
+            }
+        }
+
+        public class AsCollectionTypeMethod
+        {
+            [Fact]
+            public void ShouldCovertToListOfObjects()
+            {
+                object obj = new List<object> { "string", 2, true, "lala"};
+                var actual = obj.As<IList<object>>();
+                actual.Count.Should().Be(4);
+                actual[0].As<string>().Should().Be("string");
+                actual[1].As<string>().Should().Be("2");
+                actual[2].As<string>().Should().Be("True");
+                actual[3].As<string>().Should().Be("lala");
+            }
+
+            [Fact]
+            public void ShouldCovertToListOfListOfInts()
+            {
+                object obj = new List<object>
+                {
+                    new List<object> {1, 2, 3},
+                    new List<object> {11, 12, 13},
+                    new List<object> {21, 22, 23},
+                    new List<object> {31, 32, 33}
+                };
+                var actual = obj.AsList(x=> x.AsList(y => y.As<int>()));
+                actual.Count.Should().Be(4);
+                actual[0][0].Should().Be(1);
+                actual[1][1].Should().Be(12);
+                actual[2][2].Should().Be(23);
+                actual[3][2].Should().Be(33);
+            }
+
+            [Fact]
+            public void ShouldCovertToDictionaryOfObjects()
+            {
+                object obj = new Dictionary<string, object>{ {"key1", "string"}, { "key2", 2}, {"key3", true}, {"key4", "lala"}};
+                var actual = obj.As<IDictionary<string, object>>();
+                actual.Count.Should().Be(4);
+                actual["key1"].As<string>().Should().Be("string");
+                actual["key2"].As<string>().Should().Be("2");
+                actual["key3"].As<string>().Should().Be("True");
+                actual["key4"].As<string>().Should().Be("lala");
+            }
+        }
+
+        public class AsStructTypeMethod
+        {
+            [Fact]
+            public void ShouldConvertToNode()
+            {
+                object obj = new Node(
+                    1L, 
+                    new List<string> {"l1", "l2"}, 
+                    new Dictionary<string, object> { {"key1", "value1"}, {"key2", 2 } });
+                var actual = obj.As<INode>();
+                actual.Identity.Id.Should().Be(1);
+                actual.Labels.Count.Should().Be(2);
+                actual.Labels[0].Should().Be("l1");
+                actual.Labels[1].Should().Be("l2");
+                actual.Properties["key1"].As<string>().Should().Be("value1");
+                actual.Properties["key2"].As<string>().Should().Be("2");
+
+                obj.As<string>().Should().Be($"{obj.GetType()}");
+            }
+
+            [Fact]
+            public void ShouldConvertToRel()
+            {
+                object obj = new Relationship(1, -2, -3, "Type",
+                    new Dictionary<string, object> {{"key1", "value1"}, {"key2", 2}});
+
+                var actual = obj.As<IRelationship>();
+                actual.Identity.Id.Should().Be(1);
+                actual.Type.Should().Be("Type");
+                actual.Properties["key1"].As<string>().Should().Be("value1");
+                actual.Properties["key2"].As<string>().Should().Be("2");
+
+                obj.As<string>().Should().Be($"{obj.GetType()}");
+            }
+
+            [Fact]
+            public void ShouldConvertToPath()
+            {
+                object obj = new Path(
+                    new List<ISegment>(),
+                    new List<INode>
+                    {
+                        new Node(
+                            1L,
+                            new List<string> {"l1", "l2"},
+                            new Dictionary<string, object> {{"key1", "value1"}, {"key2", 2}})
+                    },
+                    new List<IRelationship>());
+
+                var actual = obj.As<IPath>();
+                actual.Start.Identity.Id.Should().Be(1);
+                actual.End.Identity.Id.Should().Be(1);
+                var node = actual.Nodes[0];
+                node.Properties["key1"].As<string>().Should().Be("value1");
+                node.Properties["key2"].As<string>().Should().Be("2");
+
+                obj.As<string>().Should().Be($"{obj.GetType()}");
+            }
+        }
+    }
+}
---
+++ Neo4j.Driver/Neo4j.Driver/ValueExtensions.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Neo4j.Driver
+{
+    /// <summary>
+    /// A collection of extensions to process values streamed back via Bolt
+    /// </summary>
+    public static class ValueExtensions
+    {
+        /// <summary>
+        /// A helper method to explicitly cast the value streamed back via Bolt to a local type.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Well support for one of the following types (or nullable if applies):
+        /// <see cref="short"/>,
+        /// <see cref="int"/>,
+        /// <see cref="long"/>,
+        /// <see cref="float"/>,
+        /// <see cref="double"/>,
+        /// <see cref="sbyte"/>,
+        /// <see cref="ushort"/>,
+        /// <see cref="uint"/>,
+        /// <see cref="ulong"/>,
+        /// <see cref="byte"/>,
+        /// <see cref="char"/>,
+        /// <see cref="bool"/>,
+        /// <see cref="string"/>, 
+        /// <see cref="INode"/>,
+        /// <see cref="IRelationship"/>,
+        /// <see cref="IPath"/>.
+        /// Undefined support for other types that are not listed above.
+        /// No support for user-defined types, e.g. Person, Movie.
+        /// </typeparam>
+        /// <param name="value">The value that streamed back via Bolt protocol, e.g.<see cref="IEntity.Properties"/></param>
+        /// <returns>The value of type <see cref="T"/></returns>
+        public static T As<T>(this object value)
+        {
+            if (value == null)
+            {
+                if (default(T) == null)
+                {
+                    return default(T);
+                }
+                throw new NotSupportedException($"Unsupported cast from `null` to `{typeof(T)}`");
+            }
+            if (value is T)
+            {
+                return (T) value;
+            }
+
+            // if the user want to force cast
+            var sourceType = value.GetType();
+            var targetType = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
+            if (targetType == typeof (string))
+            {
+                return value.ToString().AsItIs<T>();
+            }
+            if (targetType == typeof (short))
+            {
+                return Convert.ToInt16(value).AsItIs<T>();
+            }
+            if (targetType == typeof (int))
+            {
+                return Convert.ToInt32(value).AsItIs<T>();
+            }
+            if (targetType == typeof (long))
+            {
+                return Convert.ToInt64(value).AsItIs<T>();
+            }
+            if (targetType == typeof (float))
+            {
+                return Convert.ToSingle(value).AsItIs<T>();
+            }
+            if (targetType == typeof (double))
+            {
+                return Convert.ToDouble(value).AsItIs<T>();
+            }
+            if (targetType == typeof (sbyte))
+            {
+                return Convert.ToSByte(value).AsItIs<T>();
+            }
+            if (targetType == typeof (ulong))
+            {
+                return Convert.ToUInt64(value).AsItIs<T>();
+            }
+            if (targetType == typeof (uint))
+            {
+                return Convert.ToUInt32(value).AsItIs<T>();
+            }
+            if (targetType == typeof (ushort))
+            {
+                return Convert.ToUInt16(value).AsItIs<T>();
+            }
+            if (targetType == typeof (byte))
+            {
+                return Convert.ToByte(value).AsItIs<T>();
+            }
+            if (targetType == typeof (char))
+            {
+                return Convert.ToChar(value).AsItIs<T>();
+            }
+            if (targetType == typeof (bool))
+            {
+                return Convert.ToBoolean(value).AsItIs<T>();
+            }
+            throw new NotSupportedException($"Unsupported cast from `{sourceType}` to `{typeof(T)}`");
+        }
+        /// <summary>
+        /// A helper method to explicitly cast the value streamed back via Bolt to a list of items,
+        /// where how the items are created are defined by the mapping function provided.
+        /// </summary>
+        /// <typeparam name="TV">The type of the items in the List</typeparam>
+        /// <param name="value">The value that streamed back via Bolt protocol, e.g.<see cref="IEntity.Properties"/></param>
+        /// <param name="mapFunc">the function that how to map each list item.</param>
+        /// <returns></returns>
+        public static IList<TV> AsList<TV>(this object value, Func<object, TV> mapFunc)
+        {
+            if (value is IList<object>)
+            {
+                var list = (from object item in (IList)value select mapFunc(item)).ToList();
+                return list;
+            }
+            throw new NotSupportedException($"Unsupported cast from `{value.GetType()}` to `{typeof(IList<TV>)}`");
+        }
+
+        private static T AsItIs<T>(this object value)
+        {
+            if (value is T)
+            {
+                return (T)value;
+            }
+            throw new NotSupportedException($"Unsupported cast from `{value.GetType()}` to `{typeof(T)}`");
+        }
+    }
+}
