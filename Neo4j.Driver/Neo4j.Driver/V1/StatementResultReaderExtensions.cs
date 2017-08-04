a9f950d13dd230ac03522ac8aaad800acf196617
Zhen Li <zhen.li@neotechnology.com>
2017-08-04 12:36:06 +0200

    Enriched the extension helpers on async result

--- Neo4j.Driver/Neo4j.Driver/V1/StatementResultReaderExtensions.cs
+++ Neo4j.Driver/Neo4j.Driver/V1/StatementResultReaderExtensions.cs
@@ -16,6 +16,7 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Neo4j.Driver.Internal;
 
@@ -23,6 +24,13 @@ namespace Neo4j.Driver.V1
 {
     public static class StatementResultReaderExtensions
     {
+        /// <summary>
+        /// Return the only record in the result stream.
+        /// </summary>
+        /// <param name="result">The result stream</param>
+        /// <returns>The only record in the result stream.</returns>
+        /// <remarks>Throws <exception cref="InvalidOperationException"></exception>
+        /// if the result contains more than one record or the result is empty.</remarks>
         public static async Task<IRecord> SingleAsync(this IStatementResultReader result)
         {
             Throw.ArgumentNullException.IfNull(result, nameof(result));
@@ -41,6 +49,54 @@ namespace Neo4j.Driver.V1
             else
             {
                 throw new InvalidOperationException("The result is empty.");
+            }
+        }
+
+        /// <summary>
+        /// Pull all records in the result stream into memory and return in a list.
+        /// </summary>
+        /// <param name="result"> The result stream.</param>
+        /// <returns>A list with all records in the result stream.</returns>
+        public static async Task<IList<IRecord>> ToListAsync(this IStatementResultReader result)
+        {
+            Throw.ArgumentNullException.IfNull(result, nameof(result));
+            IList<IRecord> list = new List<IRecord>();
+            while (await result.ReadAsync().ConfigureAwait(false))
+            {
+                list.Add(result.Current());
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Read each record in the result stream and aplly the operation on each record.
+        /// </summary>
+        /// <param name="result">The result stream.</param>
+        /// <param name="operation">The operation is carried out on each record.</param>
+        /// <returns>A Task that completes when all records have been processed.</returns>
+        public static async Task ForEachAsync(this IStatementResultReader result, Action<IRecord> operation)
+        {
+            Throw.ArgumentNullException.IfNull(result, nameof(result));
+            while (await result.ReadAsync().ConfigureAwait(false))
+            {
+                var record = result.Current();
+                operation(record);
+            }
+        }
+
+        /// <summary>
+        /// Read each record in the result stream and aplly operations on each record
+        /// </summary>
+        /// <param name="result">The result stream.</param>
+        /// <param name="operation">The operation is carried out on each record.</param>
+        /// <returns>A Task that completes whe nall records have been processed.</returns>
+        public static async Task ForEachAsync(this IStatementResultReader result, Func<IRecord, Task> operation)
+        {
+            Throw.ArgumentNullException.IfNull(result, nameof(result));
+            while (await result.ReadAsync().ConfigureAwait(false))
+            {
+                var record = result.Current();
+                await operation(record).ConfigureAwait(false);
             }
         }
     }
