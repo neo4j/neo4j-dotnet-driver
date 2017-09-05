42cd3d79207f10e7b21389728fa791851c132640
Ali Ince <ali.ince@neotechnology.com>
2017-09-05 11:18:15 +0100

    for backward compatibility run tests only for net452 target till making TC changes

--- Neo4j.Driver/runTests.ps1
+++ Neo4j.Driver/runTests.ps1
@@ -6,7 +6,7 @@ If ($args.Length -ne 0)
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 
-Invoke-Expression "cd $dir\Neo4j.Driver.Tests; dotnet xunit -nobuild"
-Invoke-Expression "cd $dir\Neo4j.Driver.IntegrationTests; dotnet xunit -nobuild -parallel none"
-Invoke-Expression "cd $dir\Neo4j.Driver.Tck.Tests; dotnet xunit -nobuild -parallel none"
+Invoke-Expression "cd $dir\Neo4j.Driver.Tests; dotnet xunit -f net452 -nobuild"
+Invoke-Expression "cd $dir\Neo4j.Driver.IntegrationTests; dotnet xunit -f net452 -nobuild -parallel none"
+Invoke-Expression "cd $dir\Neo4j.Driver.Tck.Tests; dotnet xunit -f net452 -nobuild -parallel none"
 
