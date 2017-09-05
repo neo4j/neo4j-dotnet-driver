e892418580e06cd3079e0838a98eb26acbc54877
Ali Ince <ali.ince@neotechnology.com>
2017-09-05 10:09:35 +0100

    changed how tests are invoked

--- Neo4j.Driver/runTests.ps1
+++ Neo4j.Driver/runTests.ps1
@@ -6,4 +6,7 @@ If ($args.Length -ne 0)
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 
-iex "$dir\packages\xunit.runner.console.2.2.0\tools\xunit.console.exe $dir\Neo4j.Driver.Tests\bin\Debug\Neo4j.Driver.Tests.dll $dir\Neo4j.Driver.IntegrationTests\bin\Debug\Neo4j.Driver.IntegrationTests.dll $dir\Neo4j.Driver.Tck.Tests\bin\Debug\Neo4j.Driver.Tck.Tests.dll -parallel none"
+Invoke-Expression "cd $dir\Neo4j.Driver.Tests; dotnet xunit -nobuild"
+Invoke-Expression "cd $dir\Neo4j.Driver.IntegrationTests; dotnet xunit -nobuild -parallel none"
+Invoke-Expression "cd $dir\Neo4j.Driver.Tck.Tests; dotnet xunit -nobuild -parallel none"
+
