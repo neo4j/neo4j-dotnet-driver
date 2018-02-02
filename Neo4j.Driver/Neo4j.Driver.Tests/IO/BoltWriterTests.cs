8b5db7bc8b40afe109c56983d0d18b9faa95b5c1
Zhen <zhen.li@neotechnology.com>
2018-02-02 17:21:10 +0100

    Fix broken build Apparently we have a nice test to verify the init message should have the correct format. Though the test was initially with a wrong header size.

--- Neo4j.Driver/Neo4j.Driver.Tests/IO/BoltWriterTests.cs
+++ Neo4j.Driver/Neo4j.Driver.Tests/IO/BoltWriterTests.cs
@@ -39,7 +39,7 @@ namespace Neo4j.Driver.Tests.IO
                 writer.Write(new InitMessage("a", new Dictionary<string, object>()));
                 writer.Flush();
 
-                mocks.VerifyResult("00 05 B1 01 81 61 A0 00 00");
+                mocks.VerifyResult("00 05 B2 01 81 61 A0 00 00");
             }
 
             [Fact]
