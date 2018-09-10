fbd1e6b45514b00fcc99eb3c9ab6f978cfb54af0
Zhen Li <zhen.li@neotechnology.com>
2018-09-10 18:07:23 +0200

    Fixing .net core 1.1 test failure

--- Neo4j.Driver/Neo4j.Driver.IntegrationTests/Internals/BoltStubServer.cs
+++ Neo4j.Driver/Neo4j.Driver.IntegrationTests/Internals/BoltStubServer.cs
@@ -17,7 +17,9 @@
 
 using System;
 using System.IO;
+using System.Net.Sockets;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Neo4j.Driver.IntegrationTests.Internals
 {
@@ -35,11 +37,13 @@ namespace Neo4j.Driver.IntegrationTests.Internals
         }
         
         private readonly IShellCommandRunner _commandRunner;
+        private readonly TcpClient _testTcpClient = new TcpClient();
 
         private BoltStubServer(string script, int port)
         {
             _commandRunner = ShellCommandRunnerFactory.Create();
             _commandRunner.BeginRunCommand("boltstub", port.ToString(), script);
+            WaitForServer(port);
         }
 
         public static BoltStubServer Start(string script, int port)
@@ -49,6 +53,14 @@ namespace Neo4j.Driver.IntegrationTests.Internals
 
         public void Dispose()
         {
+            try
+            {
+                Disconnect(_testTcpClient);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
             _commandRunner.EndRunCommand();
         }
 
@@ -60,6 +72,62 @@ namespace Neo4j.Driver.IntegrationTests.Internals
                 throw new ArgumentException($"Cannot locate script file `{scriptFilePath}`", scriptFilePath);
             }
             return scriptFilePath;
+        }
+
+        private enum ServerStatus
+        {
+            Online, Offline
+        }
+
+        private void WaitForServer(int port, ServerStatus status = ServerStatus.Online)
+        {
+            var retryAttempts = 20;
+            for (var i = 0; i < retryAttempts; i++)
+            {
+                ServerStatus currentStatus;
+                try
+                {
+                    Connect(_testTcpClient, port);
+                    if (_testTcpClient.Connected)
+                    {
+                        currentStatus = ServerStatus.Online;
+                    }
+                    else
+                    {
+                        currentStatus = ServerStatus.Offline;
+                    }
+                }
+                catch (Exception)
+                {
+                    currentStatus = ServerStatus.Offline;
+                }
+
+                if (currentStatus == status)
+                {
+                    return;
+                }
+                // otherwise wait and retry
+                Task.Delay(300).Wait();
+            }
+            throw new InvalidOperationException($"Waited for 6s for stub server to be in {status} status, but failed.");
+        }
+
+        private void Disconnect(TcpClient testTcpClient)
+        {
+#if NET452
+            testTcpClient.Close();
+#else
+            testTcpClient.Dispose();
+#endif
+        }
+
+        private void Connect(TcpClient testTcpClient, int port)
+        {
+#if NET452
+            testTcpClient.Connect("127.0.0.1", port);
+#else
+            Task.Run(() => testTcpClient.ConnectAsync("127.0.0.1", port)).Wait();
+#endif
         }
     }
 }
