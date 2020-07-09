d1ef62349b0a6ed6a062dde7f6ac1a9672775a51
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2020-07-09 15:02:25 +0100

    Added TransactionRollback protocol object.

---
+++ Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Protocol/Transaction/TransactionRollback.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neo4j.Driver.Tests.TestBackend.Protocol.Transaction
+{
+    class TransactionRollback
+    {
+    }
+}
