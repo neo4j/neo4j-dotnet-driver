416512c0858a58caf01387c7b17d98e365d4e2da
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2020-07-09 10:55:49 +0100

    Renamed Response to ProtocolResponse to highlight its generic nature.
    Rearranged folder layout to group similar protocol objects together, such as session related, driver related etc.
    Added new protocol types
    	SessionWriteTransaction
    	SessionBeginTransaction

---
+++ Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Protocol/Session/SessionBeginTransaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Neo4j.Driver;
+
+namespace Neo4j.Driver.Tests.TestBackend
+{
+    internal class SessionBeginTransaction : IProtocolObject
+    {
+        public SessionBeginTransactionType data { get; set; } = new SessionBeginTransactionType();
+        [JsonIgnore]
+        public IAsyncTransaction Transaction { get; set; }
+
+        public class SessionBeginTransactionType
+        {
+            public string sessionId { get; set; }
+        }
+
+        public override async Task Process()
+        {
+            var sessionContainer = (NewSession)ObjManager.GetObject(data.sessionId);
+            Transaction = await sessionContainer.Session.BeginTransactionAsync();
+        }
+
+        public override string Respond()
+        {
+            return new Response("Transaction", uniqueId).Encode();
+        }
+    }
+}
