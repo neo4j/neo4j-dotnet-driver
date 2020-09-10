7f01f34e1afc7436dfe0cce2c368b62dfeefb8ef
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2020-09-10 17:50:43 +0100

    Expanded DriverError protocol response to include the error type.

--- Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Exceptions/ExceptionManager.cs
+++ Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Exceptions/ExceptionManager.cs
@@ -27,6 +27,26 @@ namespace Neo4j.Driver.Tests.TestBackend
 
     internal static class ExceptionManager
     {
+        private static Dictionary<Type, string> TypeMap { get; set; } = new Dictionary<Type, string>()
+        {
+            { typeof(Neo4jException),                   "Neo4jError" },
+            { typeof(ClientException),                  "ClientError" },
+            { typeof(TransientException),               "DriverError" },        //Should maybe Transient error, talk to Peter or Martin
+            { typeof(DatabaseException),                "DatabaseError" },
+            { typeof(ServiceUnavailableException),      "ServiceUnavailableError" },
+            { typeof(SessionExpiredException),          "SessionExpiredError" },
+            { typeof(ProtocolException),                "ProtocolError" },
+            { typeof(SecurityException),                "SecurityError" },
+            { typeof(AuthenticationException),          "AuthenticationError" },
+            { typeof(ValueTruncationException),         "ValueTruncationError" },
+            { typeof(ValueOverflowException),           "ValueOverflowError" },
+            { typeof(FatalDiscoveryException),          "FatalDiscoveryError" },
+            { typeof(ResultConsumedException),          "ResultConsumedError" },
+
+            { typeof(NotSupportedException),            "NotSupportedException" }
+        };
+
+
         internal static ProtocolResponse GenerateExceptionResponse(Exception ex)
         {
             string exceptionMessage = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message;
@@ -34,7 +54,7 @@ namespace Neo4j.Driver.Tests.TestBackend
             if (ex is Neo4jException || ex is NotSupportedException) {
                 ProtocolException newError = (ProtocolException)ProtocolObjectFactory.CreateObject(Protocol.Types.ProtocolException);
                 newError.ExceptionObj = ex;
-                return new ProtocolResponse("DriverError", new { id = newError.uniqueId, msg = exceptionMessage } );
+                return new ProtocolResponse("DriverError", new { id = newError.uniqueId, errorType = TypeMap[ex.GetType()], msg = exceptionMessage } );
             }
             Trace.WriteLine($"Exception thrown {exceptionMessage}\n{ex.StackTrace}");
             return new ProtocolResponse("BackendError", new { msg = exceptionMessage } );
