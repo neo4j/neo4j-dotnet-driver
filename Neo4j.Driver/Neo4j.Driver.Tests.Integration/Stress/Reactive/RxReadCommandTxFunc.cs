62c869a4d0ab62a4004b4d705fd6918f52228c51
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2021-01-22 11:28:43 +0000

    Changing Reactive stress tests to use transactional functions
    Added new classes:
    RxReadCommandTxFunc
    RxWriteCommandTxFunc
    RxWrongCommandTxFunc
    RxFailingCommandTxFunc
    
    CreateRxCommand now only adds the above types to the tests to be run.

---
+++ Neo4j.Driver/Neo4j.Driver.Tests.Integration/Stress/Reactive/RxReadCommandTxFunc.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) 2002-2020 "Neo4j,"
+// Neo4j Sweden AB [http://neo4j.com]
+// 
+// This file is part of Neo4j.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Neo4j.Driver.Internal;
+using static Neo4j.Driver.Tests.Assertions;
+
+namespace Neo4j.Driver.IntegrationTests.Stress
+{
+	public class RxReadCommandTxFunc<TContext> : RxCommand<TContext>
+		where TContext : StressTestContext
+	{
+		public RxReadCommandTxFunc(IDriver driver, bool useBookmark)
+			: base(driver, useBookmark)
+		{
+		}
+
+		public override async Task ExecuteAsync(TContext context)
+		{
+			var session = NewSession(AccessMode.Read, context);
+
+			var summary = await session.ReadTransaction(txc =>
+				{
+					var result = txc.Run("MATCH (n) RETURN n LIMIT 1");
+
+					return result
+						.Records()
+						.SingleOrDefaultAsync()
+						.All(r => Matches(() => r?[0].Should().BeAssignableTo<INode>()))
+						.SelectMany(_ => result.Consume())
+						.CatchAndThrow(_ => txc.Rollback<IResultSummary>())
+						.Concat(txc.Commit<IResultSummary>());
+				})
+				.CatchAndThrow(_ => session.Close<IResultSummary>())
+				.Concat(session.Close<IResultSummary>());
+
+			if (summary != null)
+			{
+				context.NodeRead(summary);
+			}
+		}
+	}
+}
+
---
+++ Neo4j.Driver/Neo4j.Driver.Tests.Integration/Stress/Reactive/RxWrongCommandTxFunc.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) 2002-2020 "Neo4j,"
+// Neo4j Sweden AB [http://neo4j.com]
+// 
+// This file is part of Neo4j.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Reactive.Testing;
+using Neo4j.Driver.Internal;
+using Xunit;
+using static Neo4j.Driver.Reactive.Utils;
+using Record = Xunit.Record;
+
+namespace Neo4j.Driver.IntegrationTests.Stress
+{
+	public class RxWrongCommandTxFunc<TContext> : RxCommand<TContext>
+		where TContext : StressTestContext
+	{
+		public RxWrongCommandTxFunc(IDriver driver)
+			: base(driver, false)
+		{
+		}
+
+		public override async Task ExecuteAsync(TContext context)
+		{
+			var session = NewSession(AccessMode.Read, context);
+
+			var result = await
+				session.ReadTransaction(txc => txc
+						.Run("RETURN")
+						.Records()
+						.CatchAndThrow(exc => txc.Rollback<IRecord>())
+						.Concat(txc.Commit<IRecord>()))
+					.CatchAndThrow(_ => session.Close<IRecord>())
+					.Concat(session.Close<IRecord>())
+					.Materialize()
+					.Select(r => new Recorded<Notification<IRecord>>(0, r))
+					.ToList();
+
+			result.AssertEqual(
+				OnError<IRecord>(0,
+					MatchesException<ClientException>(exc => exc.Message.Contains("Unexpected end of input"))));
+		}
+	}
+}
