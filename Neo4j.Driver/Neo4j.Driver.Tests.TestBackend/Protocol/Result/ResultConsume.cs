ab3c9b94fdc44f20ed9b1ce3c86a2fbfd272ea6b
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2021-02-02 14:35:38 +0000

    Adding ResultConsume to protocol and its factory

---
+++ Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Protocol/Result/ResultConsume.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Neo4j.Driver.Tests.TestBackend
+{
+	internal class ResultConsume : IProtocolObject
+	{
+		public ResultConsumeType data { get; set; } = new ResultConsumeType();
+		[JsonIgnore]
+		public IRecord Records { get; set; }
+
+		public class ResultConsumeType
+		{
+			public string resultId { get; set; }
+		}
+
+		public override async Task Process()
+		{
+			var results = ((Result)ObjManager.GetObject(data.resultId)).Results;
+			await results.ConsumeAsync().ConfigureAwait(false);
+		}
+
+		public override string Respond()
+		{
+			return new ProtocolResponse("Summary", (object)null).Encode();
+		}
+	}
+}
