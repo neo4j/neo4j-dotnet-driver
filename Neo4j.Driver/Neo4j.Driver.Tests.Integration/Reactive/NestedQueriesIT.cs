baa0da8fd860071261419c297e8980776ce8517a
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2021-02-24 09:43:32 +0000

    Update Neo4j.Driver/Neo4j.Driver.Tests.Integration/Reactive/NestedQueriesIT.cs
    Co-authored-by: Florent Biville <445792+fbiville@users.noreply.github.com>

--- Neo4j.Driver/Neo4j.Driver.Tests.Integration/Reactive/NestedQueriesIT.cs
+++ Neo4j.Driver/Neo4j.Driver.Tests.Integration/Reactive/NestedQueriesIT.cs
@@ -79,7 +79,8 @@ namespace Neo4j.Driver.IntegrationTests.Reactive
 		public bool OutputMessage(string message, string expectedMessage)
 		{
 			Output.WriteLine("Actual Message: " + message);
-			return message.Contains("Excpeted Message: " + expectedMessage);
+			return message.Contains("Expected Message: " + expectedMessage);
+
 		}
 
 		[RequireServerFact("4.0.0", GreaterThanOrEqualTo)]
