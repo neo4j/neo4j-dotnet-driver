f4c8dc2d4f79e55336960960a4b5b45d0a954303
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2021-05-05 09:13:33 +0100

    Added GetFeatures Request and FeatureList response (#522)

---
+++ Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Protocol/Handshake/GetFeatures.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neo4j.Driver.Tests.TestBackend
+{
+	internal class GetFeatures : IProtocolObject
+	{
+		public override async Task Process()
+		{
+			await Task.CompletedTask;
+		}
+
+		public override string Respond()
+		{
+			return new ProtocolResponse("FeatureList", new
+			{
+				features = SupportedFeatures.FeaturesList
+			}).Encode();
+		}
+	}
+}
---
+++ Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/SupportedFeatures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neo4j.Driver.Tests.TestBackend
+{
+	internal static class SupportedFeatures
+	{
+		public static List<string> FeaturesList { get; }
+
+		static SupportedFeatures()
+		{
+			FeaturesList.Add("AutorizationExpiredTreament");
+		}
+	}
+}
