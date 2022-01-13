f3f60618bf7d9a6e7488bcbc1221249859d05a78
grant lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2022-01-13 11:05:55 +0000

    fix testcase knowing if timeout is implicit or explicit.

--- Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Protocol/Session/SessionRun.cs
+++ Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Protocol/Session/SessionRun.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Neo4j.Driver.Tests.TestBackend
 {
+	internal class SeessionRunTypeJsonConverter : JsonConverter<SessionRun.SessionRunType>
+	{
+		public override void WriteJson(JsonWriter writer, SessionRun.SessionRunType value, JsonSerializer serializer) =>
+			throw new NotImplementedException();
+
+		public override SessionRun.SessionRunType ReadJson(JsonReader reader, Type objectType, SessionRun.SessionRunType existingValue, bool hasExistingValue,
+			JsonSerializer serializer)
+		{
+			var json = JObject.Load(reader);
+			return new SessionRun.SessionRunType
+			{
+				sessionId = json["sessionId"]?.Value<string>(),
+				cypher = json["cypher"]?.Value<string>(),
+				parameters = json["params"]?.ToObject<Dictionary<string, CypherToNativeObject>>(),
+				txMeta = json["txMeta"]?.ToObject<Dictionary<string, object>>(),
+				timeout = json["timeout"]?.Value<int?>(),
+				TimeoutSet = json.ContainsKey("timeout")
+			};
+		}
+	}
+
     internal class SessionRun : IProtocolObject
     {
         public SessionRunType data { get; set; } = new SessionRunType();
@@ -25,6 +48,9 @@ namespace Neo4j.Driver.Tests.TestBackend
             public Dictionary<string, object> txMeta { get; set; } = new Dictionary<string, object>();
 
             public int? timeout { get; set; }
+
+			[JsonIgnore]
+			public bool TimeoutSet { get; set; }
         }
 
         private Dictionary<string, object> ConvertParameters(Dictionary<string, CypherToNativeObject> source)
@@ -44,9 +70,12 @@ namespace Neo4j.Driver.Tests.TestBackend
 
         void TransactionConfig(TransactionConfigBuilder configBuilder)
         {
-            if (data.timeout.HasValue)
+            if (data.TimeoutSet)
             {
-                configBuilder.WithTimeout(TimeSpan.FromMilliseconds(data.timeout.Value));
+	            var timeout = data.timeout.HasValue
+		            ? TimeSpan.FromMilliseconds(data.timeout.Value)
+		            : default(TimeSpan?);
+				configBuilder.WithTimeout(timeout);
             }
 
             if (data.txMeta.Count > 0) configBuilder.WithMetadata(data.txMeta);
