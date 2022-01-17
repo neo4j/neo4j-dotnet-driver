9f13ea3d4f92e948cfc4da2d23a4e359ab9473eb
grant lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2022-01-17 11:29:13 +0000

    remove unused namespaces

--- Neo4j.Driver/Neo4j.Driver/Internal/MessageHandling/V5_0/HelloResponseHandler.cs
+++ Neo4j.Driver/Neo4j.Driver/Internal/MessageHandling/V5_0/HelloResponseHandler.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using Neo4j.Driver.Internal.Connector;
-using Neo4j.Driver.Internal.MessageHandling.Metadata;
+﻿using Neo4j.Driver.Internal.Connector;
 using Neo4j.Driver.Internal.Protocol;
-using Neo4j.Driver.Internal.Util;
 
 namespace Neo4j.Driver.Internal.MessageHandling.V5_0
 {
