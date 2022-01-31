7f87625387bb8a7bffc800b1eb560fb932ca2e57
grant lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2022-01-31 13:29:16 +0000

    switch some tabs to spaces to trigger a build against updated testkit

--- Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Exceptions/ExceptionManager.cs
+++ Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Exceptions/ExceptionManager.cs
@@ -28,52 +28,52 @@ namespace Neo4j.Driver.Tests.TestBackend
     internal static class ExceptionManager
     {
         private static Dictionary<Type, string> TypeMap { get; set; } = new Dictionary<Type, string>()
-		{
-			{ typeof(Neo4jException),                   "Neo4jError" },
-			{ typeof(ClientException),                  "ClientError" },
-			{ typeof(TransientException),               "DriverError" },        //Should maybe Transient error, talk to Peter or Martin
+        {
+            { typeof(Neo4jException),                   "Neo4jError" },
+            { typeof(ClientException),                  "ClientError" },
+            { typeof(TransientException),               "DriverError" },        //Should maybe Transient error, talk to Peter or Martin
             { typeof(DatabaseException),                "DatabaseError" },
-			{ typeof(ServiceUnavailableException),      "ServiceUnavailableError" },
-			{ typeof(SessionExpiredException),          "SessionExpiredError" },
-			{ typeof(Driver.ProtocolException),         "ProtocolError" },
-			{ typeof(SecurityException),                "SecurityError" },
-			{ typeof(AuthenticationException),          "AuthenticationError" },
-			{ typeof(AuthorizationException),           "AuthorizationExpired" },
-			{ typeof(ValueTruncationException),         "ValueTruncationError" },
-			{ typeof(ValueOverflowException),           "ValueOverflowError" },
-			{ typeof(FatalDiscoveryException),          "FatalDiscoveryError" },
-			{ typeof(ResultConsumedException),          "ResultConsumedError" },
-			{ typeof(TransactionNestingException),      "TransactionNestingException" },
-			{ typeof(TokenExpiredException),			"TokenExpiredError"  },
-			{ typeof(ConnectionReadTimeoutException),   "ConnectionReadTimeoutError"},
-			{ typeof(InvalidBookmarkException),			"InvalidBookmarkError"},
+            { typeof(ServiceUnavailableException),      "ServiceUnavailableError" },
+            { typeof(SessionExpiredException),          "SessionExpiredError" },
+            { typeof(Driver.ProtocolException),         "ProtocolError" },
+            { typeof(SecurityException),                "SecurityError" },
+            { typeof(AuthenticationException),          "AuthenticationError" },
+            { typeof(AuthorizationException),           "AuthorizationExpired" },
+            { typeof(ValueTruncationException),         "ValueTruncationError" },
+            { typeof(ValueOverflowException),           "ValueOverflowError" },
+            { typeof(FatalDiscoveryException),          "FatalDiscoveryError" },
+            { typeof(ResultConsumedException),          "ResultConsumedError" },
+            { typeof(TransactionNestingException),      "TransactionNestingException" },
+            { typeof(TokenExpiredException),			"TokenExpiredError"  },
+            { typeof(ConnectionReadTimeoutException),   "ConnectionReadTimeoutError"},
+            { typeof(InvalidBookmarkException),			"InvalidBookmarkError"},
 
             { typeof(NotSupportedException),            "NotSupportedException" },
 
-			{ typeof(ArgumentException),				"ArgumentError"}
+            { typeof(ArgumentException),				"ArgumentError"}
         };
 
 
         internal static ProtocolResponse GenerateExceptionResponse(Exception ex)
         {
-			string outerExceptionMessage = ex.Message;
+            string outerExceptionMessage = ex.Message;
             string exceptionMessage = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message;
-			var type = TypeMap.GetValueOrDefault(ex.GetType());
+            var type = TypeMap.GetValueOrDefault(ex.GetType());
 
 
-			//if (ex is Neo4jException || ex is NotSupportedException)
-			if(type is not null)
-			{
+            //if (ex is Neo4jException || ex is NotSupportedException)
+            if(type is not null)
+            {
                 ProtocolException newError = ProtocolObjectFactory.CreateObject<ProtocolException>();
                 newError.ExceptionObj = ex;
-				string errorCode = (ex is Neo4jException) ? ((Neo4jException)ex).Code : type;
-				return new ProtocolResponse("DriverError", new
-				{
-					id = newError.uniqueId,
-					errorType = type,
-					msg = exceptionMessage,
-					code = errorCode
-				});
+                string errorCode = (ex is Neo4jException) ? ((Neo4jException)ex).Code : type;
+                return new ProtocolResponse("DriverError", new
+                {
+                    id = newError.uniqueId,
+                    errorType = type,
+                    msg = exceptionMessage,
+                    code = errorCode
+                });
             }
             else if (ex is DriverExceptionWrapper)
             {
