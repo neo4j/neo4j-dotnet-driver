1387046bc60d660d8d709d2b5be8906a7a81f721
grant lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2022-02-08 12:42:20 +0000

    remove mixed whitespace for build against new testkit version

--- Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Protocol/Driver/NewDriver.cs
+++ Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Protocol/Driver/NewDriver.cs
@@ -6,25 +6,25 @@ using Newtonsoft.Json;
 namespace Neo4j.Driver.Tests.TestBackend
 {
     internal class NewDriver : IProtocolObject
-	{
-		public NewDriverType data { get; set; } = new NewDriverType();
-		[JsonIgnore]
-		public IDriver Driver { get; set; }
-		[JsonIgnore]
-		private Controller Control { get; set; }
+    {
+        public NewDriverType data { get; set; } = new NewDriverType();
+        [JsonIgnore]
+        public IDriver Driver { get; set; }
+        [JsonIgnore]
+        private Controller Control { get; set; }
 
 
-		public class NewDriverType
-		{
+        public class NewDriverType
+        {
             private string[] _trustedCertificates;
             public string uri { get; set; }
-			public AuthorizationToken authorizationToken { get; set; } = new AuthorizationToken();
-			public string userAgent { get; set; }
-			public bool resolverRegistered { get; set; } = false;
-			public bool domainNameResolverRegistered { get; set; } = false;
-			public int connectionTimeoutMs { get; set; } = -1;
-			public int? maxConnectionPoolSize { get; set; }
-			public int? connectionAcquisitionTimeoutMs { get; set; }
+            public AuthorizationToken authorizationToken { get; set; } = new AuthorizationToken();
+            public string userAgent { get; set; }
+            public bool resolverRegistered { get; set; } = false;
+            public bool domainNameResolverRegistered { get; set; } = false;
+            public int connectionTimeoutMs { get; set; } = -1;
+            public int? maxConnectionPoolSize { get; set; }
+            public int? connectionAcquisitionTimeoutMs { get; set; }
             [JsonIgnore]
             public bool ModifiedTrustedCertificates = false;
             public string[] trustedCertificates
@@ -38,59 +38,59 @@ namespace Neo4j.Driver.Tests.TestBackend
             }
 
             public bool? encrypted { get; set; }
-		}
+        }
 
-		public override async Task Process(Controller controller)
-		{
-			Control = controller;
-			var authTokenData = data.authorizationToken.data;
+        public override async Task Process(Controller controller)
+        {
+            Control = controller;
+            var authTokenData = data.authorizationToken.data;
 
-			IAuthToken authToken;
+            IAuthToken authToken;
 
-			switch (authTokenData.scheme)
-			{
-				case "bearer":
-					authToken = AuthTokens.Bearer(authTokenData.credentials);
-					break;
-				case "kerberos":
-					authToken = AuthTokens.Kerberos(authTokenData.credentials);
-					break;
+            switch (authTokenData.scheme)
+            {
+                case "bearer":
+                    authToken = AuthTokens.Bearer(authTokenData.credentials);
+                    break;
+                case "kerberos":
+                    authToken = AuthTokens.Kerberos(authTokenData.credentials);
+                    break;
 
-				default:
-					authToken = AuthTokens.Custom(authTokenData.principal,
-												  authTokenData.credentials,
-												  authTokenData.realm,
-												  authTokenData.scheme,
-												  authTokenData.parameters);
-					break;
+                default:
+                    authToken = AuthTokens.Custom(authTokenData.principal,
+                                                  authTokenData.credentials,
+                                                  authTokenData.realm,
+                                                  authTokenData.scheme,
+                                                  authTokenData.parameters);
+                    break;
 
-			}
+            }
 
-			Driver = GraphDatabase.Driver(data.uri, authToken, DriverConfig);
+            Driver = GraphDatabase.Driver(data.uri, authToken, DriverConfig);
 
-			await Task.CompletedTask;
-		}
+            await Task.CompletedTask;
+        }
 
-		public override string Respond()
-		{
-			return new ProtocolResponse("Driver", uniqueId).Encode();
-		}
+        public override string Respond()
+        {
+            return new ProtocolResponse("Driver", uniqueId).Encode();
+        }
 
-		private void DriverConfig(ConfigBuilder configBuilder)
-		{
-			if (!string.IsNullOrEmpty(data.userAgent))
+        private void DriverConfig(ConfigBuilder configBuilder)
+        {
+            if (!string.IsNullOrEmpty(data.userAgent))
                 configBuilder.WithUserAgent(data.userAgent);
 
-			if (data.resolverRegistered)
+            if (data.resolverRegistered)
                 configBuilder.WithResolver(new ListAddressResolver(Control, data.uri));
 
-			if (data.connectionTimeoutMs > 0)
+            if (data.connectionTimeoutMs > 0)
                 configBuilder.WithConnectionTimeout(TimeSpan.FromMilliseconds(data.connectionTimeoutMs));
 
-			if (data.maxConnectionPoolSize.HasValue)
+            if (data.maxConnectionPoolSize.HasValue)
                 configBuilder.WithMaxConnectionPoolSize(data.maxConnectionPoolSize.Value);
 
-			if (data.connectionAcquisitionTimeoutMs.HasValue)
+            if (data.connectionAcquisitionTimeoutMs.HasValue)
                 configBuilder.WithConnectionAcquisitionTimeout(
                     TimeSpan.FromMilliseconds(data.connectionAcquisitionTimeoutMs.Value));
 
@@ -111,7 +111,7 @@ namespace Neo4j.Driver.Tests.TestBackend
 
             SimpleLogger logger = new SimpleLogger();
 
-			configBuilder.WithLogger(logger);
-		}
-	}
+            configBuilder.WithLogger(logger);
+        }
+    }
 }
