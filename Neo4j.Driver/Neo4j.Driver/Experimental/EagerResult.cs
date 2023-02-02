3c8ff9030cbb84b458d33e796e3cd7e5032a2b5d
Richard Irons <richard.irons@neo4j.com>
2023-02-02 09:13:33 +0000

    Licensing text

--- Neo4j.Driver/Neo4j.Driver/Experimental/EagerResult.cs
+++ Neo4j.Driver/Neo4j.Driver/Experimental/EagerResult.cs
@@ -1,22 +1,19 @@
-﻿// Copyright (c) 2002-2022 "Neo4j,"
+﻿// Copyright (c) "Neo4j"
 // Neo4j Sweden AB [http://neo4j.com]
-// 
+//
 // This file is part of Neo4j.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
+//
+// Licensed under the Apache License, Version 2.0 (the "License"):
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-using System.Collections;
-using System.Collections.Generic;
 
 namespace Neo4j.Driver.Experimental;
 
@@ -34,32 +31,8 @@ public sealed class EagerResult<T>
     }
 
     /// <summary>
-    /// Deconstructs the result into its constituent parts.
-    /// </summary>
-    /// <param name="result">The result returned from the query.</param>
-    /// <param name="summary">The summary of the result.</param>
-    /// <param name="keys">The keys present in the result.</param>
-    public void Deconstruct(out T result, out IResultSummary summary, out string[] keys)
-    {
-        keys = Keys;
-        result = Result;
-        summary = Summary;
-    }
-
-    /// <summary>
-    /// Deconstructs the result into its constituent parts.
-    /// </summary>
-    /// <param name="result">The result returned from the query.</param>
-    /// <param name="summary">The summary of the result.</param>
-    public void Deconstruct(out T result, out IResultSummary summary)
-    {
-        result = Result;
-        summary = Summary;
-    }
-
-    /// <summary>
     /// There is no guarantee that anything in Neo4j.Driver.Experimental namespace will be in a next minor version.
-    /// Least common set of fields in <see cref="Result"/>.
+    /// Least common set of fields in <see cref="Result" />.
     /// </summary>
     public string[] Keys { get; init; }
 
@@ -74,4 +47,24 @@ public sealed class EagerResult<T>
     /// Query summary.
     /// </summary>
     public IResultSummary Summary { get; init; }
+
+    /// <summary>Deconstructs the result into its constituent parts.</summary>
+    /// <param name="result">The result returned from the query.</param>
+    /// <param name="summary">The summary of the result.</param>
+    /// <param name="keys">The keys present in the result.</param>
+    public void Deconstruct(out T result, out IResultSummary summary, out string[] keys)
+    {
+        keys = Keys;
+        result = Result;
+        summary = Summary;
+    }
+
+    /// <summary>Deconstructs the result into its constituent parts.</summary>
+    /// <param name="result">The result returned from the query.</param>
+    /// <param name="summary">The summary of the result.</param>
+    public void Deconstruct(out T result, out IResultSummary summary)
+    {
+        result = Result;
+        summary = Summary;
+    }
 }
