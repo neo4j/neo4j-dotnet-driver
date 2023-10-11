2688993e018a91fb60fdbaae99de1ef461b650b7
grant lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2023-10-11 23:09:23 +0100

    Attempt to speed up testkit

--- testkit/build.py
+++ testkit/build.py
@@ -10,7 +10,4 @@ def run(args):
         args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
 
 if __name__ == "__main__":
-    run(["dotnet", "restore", "--disable-parallel", "-v", "n", "Neo4j.Driver/Neo4j.Driver.sln"])
-    run(["dotnet", "clean", "./Neo4j.Driver/Neo4j.Driver.sln"])
-    run(["dotnet", "build", "./Neo4j.Driver/Neo4j.Driver.sln"])
-    run(["dotnet", "publish", "./Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Neo4j.Driver.Tests.TestBackend.csproj", "--self-contained", "false", "--output", "./bin/Publish"])
+    run(["dotnet", "publish", "./Neo4j.Driver/Neo4j.Driver.Tests.TestBackend/Neo4j.Driver.Tests.TestBackend.csproj", "-c", "Release", "--self-contained", "false", "--output", "./bin/Publish"])
