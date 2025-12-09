d03fba571cbbf60b2bd28fdf2fa3d2002f302a67
Richard Irons <richard.irons@neo4j.com>
2025-12-09 16:16:31 +0000

    Unified vector stringer with tests

--- Neo4j.Driver/Neo4j.Driver.Tests/Types/VectorTests.cs
+++ Neo4j.Driver/Neo4j.Driver.Tests/Types/VectorTests.cs
@@ -91,4 +91,126 @@ public class VectorTests
     {
         Assert.Throws<ArgumentException>(() => _ = new Vector<int>(null));
     }
+
+    [Theory]
+    [InlineData(new sbyte[] { }, "vector([], 0, INTEGER8 NOT NULL)")]
+    [InlineData(new sbyte[] { 0 }, "vector([0], 1, INTEGER8 NOT NULL)")]
+    [InlineData(new sbyte[] { 1, -2, 127 }, "vector([1, -2, 127], 3, INTEGER8 NOT NULL)")]
+    public void ToString_ReturnsCorrectFormat_SByte(sbyte[] values, string expected)
+    {
+        var vector = new Vector<sbyte>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new short[] { }, "vector([], 0, INTEGER16 NOT NULL)")]
+    [InlineData(new short[] { 0, 100, -32768 }, "vector([0, 100, -32768], 3, INTEGER16 NOT NULL)")]
+    public void ToString_ReturnsCorrectFormat_Short(short[] values, string expected)
+    {
+        var vector = new Vector<short>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new int[] { }, "vector([], 0, INTEGER32 NOT NULL)")]
+    [InlineData(new[] { 42, -1000 }, "vector([42, -1000], 2, INTEGER32 NOT NULL)")]
+    public void ToString_ReturnsCorrectFormat_Int(int[] values, string expected)
+    {
+        var vector = new Vector<int>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new long[] { }, "vector([], 0, INTEGER NOT NULL)")]
+    [InlineData(new[] { 0, 9223372036854775807L }, "vector([0, 9223372036854775807], 2, INTEGER NOT NULL)")]
+    public void ToString_ReturnsCorrectFormat_Long(long[] values, string expected)
+    {
+        var vector = new Vector<long>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new float[] { }, "vector([], 0, FLOAT32 NOT NULL)")]
+    [InlineData(new[] { 0.5f }, "vector([0.5], 1, FLOAT32 NOT NULL)")]
+    [InlineData(new[] { -1.5f, 3f }, "vector([-1.5, 3], 2, FLOAT32 NOT NULL)")]
+    public void ToString_ReturnsCorrectFormat_Float(float[] values, string expected)
+    {
+        var vector = new Vector<float>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new double[] { }, "vector([], 0, FLOAT NOT NULL)")]
+    [InlineData(new[] { 0.0 }, "vector([0], 1, FLOAT NOT NULL)")]
+    [InlineData(new[] { 1.23, -4.56 }, "vector([1.23, -4.56], 2, FLOAT NOT NULL)")]
+    public void ToString_ReturnsCorrectFormat_Double(double[] values, string expected)
+    {
+        var vector = new Vector<double>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new sbyte[] { }, "vector([], 0, INTEGER8 NOT NULL)")]
+    public void ToString_HandlesSpecialFloatValues_SByte(sbyte[] values, string expected)
+    {
+        var vector = new Vector<sbyte>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new short[] { }, "vector([], 0, INTEGER16 NOT NULL)")]
+    public void ToString_HandlesSpecialFloatValues_Short(short[] values, string expected)
+    {
+        var vector = new Vector<short>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new int[] { }, "vector([], 0, INTEGER32 NOT NULL)")]
+    public void ToString_HandlesSpecialFloatValues_Int(int[] values, string expected)
+    {
+        var vector = new Vector<int>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new long[] { }, "vector([], 0, INTEGER NOT NULL)")]
+    public void ToString_HandlesSpecialFloatValues_Long(long[] values, string expected)
+    {
+        var vector = new Vector<long>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new[] { float.NaN }, "vector([NaN], 1, FLOAT32 NOT NULL)")]
+    [InlineData(new[] { float.PositiveInfinity }, "vector([Infinity], 1, FLOAT32 NOT NULL)")]
+    [InlineData(new[] { float.NegativeInfinity }, "vector([-Infinity], 1, FLOAT32 NOT NULL)")]
+    public void ToString_HandlesSpecialFloatValues_Float(float[] values, string expected)
+    {
+        var vector = new Vector<float>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new[] { double.NaN }, "vector([NaN], 1, FLOAT NOT NULL)")]
+    [InlineData(new[] { double.PositiveInfinity }, "vector([Infinity], 1, FLOAT NOT NULL)")]
+    [InlineData(new[] { double.NegativeInfinity }, "vector([-Infinity], 1, FLOAT NOT NULL)")]
+    [InlineData(new[] { double.NaN, double.PositiveInfinity, double.NegativeInfinity }, "vector([NaN, Infinity, -Infinity], 3, FLOAT NOT NULL)")]
+    public void ToString_HandlesSpecialFloatValues_Double(double[] values, string expected)
+    {
+        var vector = new Vector<double>(values);
+        var result = vector.ToString();
+        result.Should().Be(expected);
+    }
 }
---
+++ Neo4j.Driver/Neo4j.Driver/Public/Types/Vector_T.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) "Neo4j"
+// Neo4j Sweden AB [https://neo4j.com]
+// 
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Neo4j.Driver;
+
+/// <summary>
+/// Represents a mathematical vector with elements of a specific supported numeric type.
+/// </summary>
+/// <typeparam name="T">
+/// The type of the vector elements. Must be one of the supported numeric types: <see cref="float"/>,
+/// <see cref="double"/>, <see cref="sbyte"/>, <see cref="short"/>, <see cref="int"/>, or <see cref="long"/>.
+/// </typeparam>
+public class Vector<T> : Vector, IVector<T> where T : struct
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Vector{T}"/> class.
+    /// </summary>
+    /// <exception cref="NotSupportedException">
+    /// Thrown if <typeparamref name="T"/> is not a supported numeric type.
+    /// </exception>
+    public Vector()
+    {
+        if (!IsSupported(typeof(T)))
+        {
+            throw new NotSupportedException($"Type {typeof(T).Name} is not supported for Vector.");
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Vector{T}"/> class with the specified values.
+    /// </summary>
+    /// <param name="values">The array of values to initialize the vector with. Must not be null or empty.</param>
+    /// <param name="originalByteStream">The original byte stream from which the vector was deserialized, if applicable.</param>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="values"/> is null or empty.</exception>
+    /// <exception cref="NotSupportedException">Thrown if <typeparamref name="T"/> is not a supported numeric type.</exception>
+    public Vector(T[] values, byte[] originalByteStream = null) : this()
+    {
+        Values = values ?? throw new ArgumentException("Values cannot be null.", nameof(values));
+        OriginalByteStream = originalByteStream;
+        UntypedValues = Values.Select(x => (object)x);
+    }
+
+    /// <summary>
+    /// Gets the array of values contained in the vector.
+    /// </summary>
+    public IReadOnlyList<T> Values { get; }
+
+    /// <inheritdoc />
+    public override Type ElementType => typeof(T);
+
+    /// <inheritdoc />
+    public IEnumerator<T> GetEnumerator() => Values.GetEnumerator();
+
+    /// <inheritdoc />
+    public override int Count => Values.Count;
+
+    /// <inheritdoc />
+    public T this[int index] => Values[index];
+
+    /// <inheritdoc />
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
