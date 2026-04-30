00ebd406d19545d270f318731802cdda6a92ae90
Richard Irons <richard.irons@neo4j.com>
2026-04-30 17:31:04 +0100

    First commit of scoped service resolver

---
+++ Neo4j.Driver/Neo4j.Driver.Tests/Internal/DependencyInjection/ScopedContainerTests.cs
@@ -0,0 +1,381 @@
+﻿// Copyright (c) "Neo4j"
+// Neo4j Sweden AB [https://neo4j.com]
+// 
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using Moq.AutoMock;
+using Neo4j.Driver.Internal.DependencyInjection;
+using Xunit;
+
+namespace Neo4j.Driver.Tests.Internal.DependencyInjection;
+
+public class ScopedContainerTests
+{
+    public interface ITestService
+    {
+    }
+
+    public interface IDependency
+    {
+    }
+
+    public interface IMultiImplementer
+    {
+    }
+
+    public class TestService : ITestService
+    {
+        public IDependency Dependency { get; }
+        public TestService(IDependency dependency) => Dependency = dependency;
+    }
+
+    public class ServiceWithNoDependencies : ITestService
+    {
+        public ServiceWithNoDependencies()
+        {
+        }
+    }
+
+    public class MultiImplementer1 : IMultiImplementer
+    {
+    }
+
+    public class MultiImplementer2 : IMultiImplementer
+    {
+    }
+
+    public class ServiceWithMultipleConstructors
+    {
+        public int ConstructorCalled { get; }
+        public ServiceWithMultipleConstructors() => ConstructorCalled = 0;
+        public ServiceWithMultipleConstructors(IDependency dependency) => ConstructorCalled = 1;
+        public ServiceWithMultipleConstructors(IDependency dep1, ITestService dep2) => ConstructorCalled = 2;
+    }
+
+    public class CircularA
+    {
+        public CircularA(CircularB b)
+        {
+        }
+    }
+
+    public class CircularB
+    {
+        public CircularB(CircularA a)
+        {
+        }
+    }
+
+    public class DisposableService : IDisposable
+    {
+        public bool IsDisposed { get; private set; }
+        public void Dispose() => IsDisposed = true;
+    }
+
+    [Fact]
+    public void RegisterInstance_ResolvesRegisteredInstance()
+    {
+        var container = new ScopedContainer();
+        var mocker = new AutoMocker();
+        var instance = mocker.Get<ITestService>();
+
+        container.RegisterInstance(instance);
+        var resolved = container.Resolve<ITestService>();
+
+        resolved.Should().BeSameAs(instance);
+    }
+
+    [Fact]
+    public void RegisterType_CreatesNewInstanceWithConstructorResolution()
+    {
+        var container = new ScopedContainer();
+        var mocker = new AutoMocker();
+        var dependency = mocker.Get<IDependency>();
+
+        container.RegisterInstance(dependency);
+        container.RegisterType<ITestService, TestService>();
+
+        var resolved = container.Resolve<ITestService>();
+
+        resolved.Should().BeOfType<TestService>();
+        ((TestService)resolved).Dependency.Should().BeSameAs(dependency);
+    }
+
+    [Fact]
+    public void RegisterType_WithNoConstructorDependencies_CreatesInstance()
+    {
+        var container = new ScopedContainer();
+
+        container.RegisterType<ITestService, ServiceWithNoDependencies>();
+        var resolved = container.Resolve<ITestService>();
+
+        resolved.Should().BeOfType<ServiceWithNoDependencies>();
+    }
+
+    [Fact]
+    public void RegisterType_SelectsConstructorWithMostParameters()
+    {
+        var container = new ScopedContainer();
+        var mocker = new AutoMocker();
+
+        container.RegisterInstance(mocker.Get<IDependency>());
+        container.RegisterInstance(mocker.Get<ITestService>());
+        container.RegisterType<ServiceWithMultipleConstructors>();
+
+        var resolved = container.Resolve<ServiceWithMultipleConstructors>();
+
+        resolved.ConstructorCalled.Should().Be(2);
+    }
+
+    [Fact]
+    public void Resolve_ThrowsForUnregisteredService()
+    {
+        var container = new ScopedContainer();
+
+        var act = () => container.Resolve<ITestService>();
+
+        act.Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("*not registered*");
+    }
+
+    [Fact]
+    public void Resolve_ThrowsForCircularDependency()
+    {
+        var container = new ScopedContainer();
+        container.RegisterType<CircularA>();
+        container.RegisterType<CircularB>();
+
+        var act = () => container.Resolve<CircularA>();
+
+        act.Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("*Circular dependency*");
+    }
+
+    [Fact]
+    public void ResolveEnumerable_ResolvesAllImplementers()
+    {
+        var container = new ScopedContainer();
+        container.RegisterType<IMultiImplementer, MultiImplementer1>();
+        container.RegisterType<IMultiImplementer, MultiImplementer2>();
+
+        var resolved = container.Resolve<IEnumerable<IMultiImplementer>>().ToArray();
+
+        resolved.Should().HaveCount(2);
+        resolved.Should().ContainSingle(x => x is MultiImplementer1);
+        resolved.Should().ContainSingle(x => x is MultiImplementer2);
+    }
+
+    [Fact]
+    public void ResolveEnumerable_ReturnsEmptyForNoImplementers()
+    {
+        var container = new ScopedContainer();
+
+        var resolved = container.Resolve<IEnumerable<IMultiImplementer>>();
+
+        resolved.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void RegisterPlugin_CallsPluginDuringResolution()
+    {
+        var container = new ScopedContainer();
+        var mocker = new AutoMocker();
+        var mockPlugin = mocker.GetMock<IResolverOverride>();
+        var overrideInstance = mocker.Get<ITestService>();
+
+        mockPlugin
+            .Setup(x => x.TryResolve(
+                typeof(ITestService),
+                It.IsAny<Type>(),
+                It.IsAny<IServiceResolver>(),
+                out It.Ref<object>.IsAny))
+            .Returns(
+                new TryResolveDelegate((_, _, _, out service) =>
+                {
+                    service = overrideInstance;
+                    return true;
+                }));
+
+        container.RegisterPlugin(mockPlugin.Object);
+        var resolved = container.Resolve<ITestService>();
+
+        resolved.Should().BeSameAs(overrideInstance);
+    }
+
+    private delegate bool TryResolveDelegate(
+        Type serviceType,
+        Type requestingType,
+        IServiceResolver resolver,
+        out object service);
+
+    [Fact]
+    public void RegisterPlugin_FallsBackToNormalResolutionWhenPluginReturnsFalse()
+    {
+        var container = new ScopedContainer();
+        var mocker = new AutoMocker();
+        var mockPlugin = mocker.GetMock<IResolverOverride>();
+        var instance = mocker.Get<ITestService>();
+
+        mockPlugin
+            .Setup(x => x.TryResolve(
+                It.IsAny<Type>(),
+                It.IsAny<Type>(),
+                It.IsAny<IServiceResolver>(),
+                out It.Ref<object>.IsAny))
+            .Returns(false);
+
+        container.RegisterPlugin(mockPlugin.Object);
+        container.RegisterInstance(instance);
+        var resolved = container.Resolve<ITestService>();
+
+        resolved.Should().BeSameAs(instance);
+    }
+
+    [Fact]
+    public void CreateScope_CreatesChildContainerWithParentDelegation()
+    {
+        var container = new ScopedContainer();
+        var mocker = new AutoMocker();
+        var parentInstance = mocker.Get<ITestService>();
+
+        container.RegisterInstance(parentInstance);
+        var scope = container.CreateChildScope();
+
+        var resolved = scope.Resolve<ITestService>();
+
+        resolved.Should().BeSameAs(parentInstance);
+    }
+
+    [Fact]
+    public void CreateScope_ChildRegistrationsDoNotAffectParent()
+    {
+        var container = new ScopedContainer();
+        var mocker = new AutoMocker();
+        var childInstance = mocker.Get<ITestService>();
+
+        var scope = (ScopedContainer)container.CreateChildScope();
+        scope.RegisterInstance(childInstance);
+
+        var act = () => container.Resolve<ITestService>();
+
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void CreateScope_ChildOverridesParentRegistration()
+    {
+        var container = new ScopedContainer();
+        var mocker = new AutoMocker();
+        var parentInstance = mocker.Get<ITestService>();
+        var childInstance = new Mock<ITestService>().Object;
+
+        container.RegisterInstance(parentInstance);
+        var scope = (ScopedContainer)container.CreateChildScope();
+        scope.RegisterInstance(childInstance);
+
+        var resolved = scope.Resolve<ITestService>();
+
+        resolved.Should().BeSameAs(childInstance);
+    }
+
+    [Fact]
+    public void ResolveEnumerable_IncludesParentAndChildImplementers()
+    {
+        var container = new ScopedContainer();
+        container.RegisterType<IMultiImplementer, MultiImplementer1>();
+
+        var scope = (ScopedContainer)container.CreateChildScope();
+        scope.RegisterType<IMultiImplementer, MultiImplementer2>();
+
+        var resolved = scope.Resolve<IEnumerable<IMultiImplementer>>().ToArray();
+
+        resolved.Should().HaveCount(2);
+        resolved.Should().ContainSingle(x => x is MultiImplementer1);
+        resolved.Should().ContainSingle(x => x is MultiImplementer2);
+    }
+
+    [Fact]
+    public void Dispose_DisposesCreatedDisposableInstances()
+    {
+        var container = new ScopedContainer();
+        container.RegisterType<DisposableService>();
+
+        var resolved = container.Resolve<DisposableService>();
+        container.Dispose();
+
+        resolved.IsDisposed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Dispose_DoesNotDisposeRegisteredInstances()
+    {
+        var container = new ScopedContainer();
+        var instance = new DisposableService();
+
+        container.RegisterInstance(instance);
+        container.Dispose();
+
+        instance.IsDisposed.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData(typeof(ITestService))]
+    [InlineData(typeof(IDependency))]
+    public void Resolve_ThrowsAfterDispose(Type serviceType)
+    {
+        var container = new ScopedContainer();
+        container.Dispose();
+
+        var act = () => container.Resolve(serviceType);
+
+        act.Should().Throw<ObjectDisposedException>();
+    }
+
+    [Fact]
+    public void CreateScope_ThrowsAfterDispose()
+    {
+        var container = new ScopedContainer();
+        container.Dispose();
+
+        var act = () => container.CreateChildScope();
+
+        act.Should().Throw<ObjectDisposedException>();
+    }
+
+    [Fact]
+    public void RegisterType_WithTypeWithNoPublicConstructor_ThrowsOnResolve()
+    {
+        var container = new ScopedContainer();
+        container.RegisterType<PrivateConstructorService>();
+
+        var act = () => container.Resolve<PrivateConstructorService>();
+
+        act.Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("*no public constructors*");
+    }
+
+    private class PrivateConstructorService
+    {
+        private PrivateConstructorService()
+        {
+        }
+    }
+}
---
+++ Neo4j.Driver/Neo4j.Driver/Internal/DependencyInjection/ScopedContainer.cs
@@ -0,0 +1,278 @@
+﻿// Copyright (c) "Neo4j"
+// Neo4j Sweden AB [https://neo4j.com]
+// 
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#nullable enable
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace Neo4j.Driver.Internal.DependencyInjection;
+
+internal class ScopedContainer : IResolutionScope, IDisposable
+{
+    private readonly Dictionary<Type, List<Registration>> _registrations = new();
+    private readonly List<IResolverOverride> _overrides = [];
+    private readonly IServiceResolver? _parent;
+    private readonly HashSet<object> _disposables = [];
+    private readonly ThreadLocal<HashSet<Type>> _resolutionStack = new(() => []);
+    private bool _disposed;
+
+    public ScopedContainer() : this(null)
+    {
+    }
+
+    private ScopedContainer(IServiceResolver? parent)
+    {
+        _parent = parent;
+    }
+
+    public void RegisterInstance<TService>(TService instance)
+    {
+        if (instance == null)
+        {
+            throw new ArgumentException("Instance cannot be null", nameof(instance));
+        }
+
+        var serviceType = typeof(TService);
+        if (!_registrations.TryGetValue(serviceType, out var registrations))
+        {
+            registrations = [];
+            _registrations[serviceType] = registrations;
+        }
+
+        registrations.Add(new Registration(instance));
+    }
+
+    public void RegisterType<TService, TImplementation>() where TImplementation : TService
+    {
+        var serviceType = typeof(TService);
+        if (!_registrations.TryGetValue(serviceType, out var registrations))
+        {
+            registrations = [];
+            _registrations[serviceType] = registrations;
+        }
+
+        registrations.Add(new Registration(typeof(TImplementation)));
+    }
+
+    public void RegisterType<TService>()
+    {
+        var serviceType = typeof(TService);
+        if (!_registrations.TryGetValue(serviceType, out var registrations))
+        {
+            registrations = [];
+            _registrations[serviceType] = registrations;
+        }
+
+        registrations.Add(new Registration(serviceType));
+    }
+
+    public void RegisterPlugin(IResolverOverride resolverOverride)
+    {
+        _overrides.Add(resolverOverride);
+    }
+
+    public TService Resolve<TService>()
+    {
+        return (TService)Resolve(typeof(TService), null);
+    }
+
+    public object Resolve(Type serviceType)
+    {
+        return Resolve(serviceType, null);
+    }
+
+    public object Resolve(Type serviceType, Type? requestingType)
+    {
+        if (_disposed)
+        {
+            throw new ObjectDisposedException(nameof(ScopedContainer));
+        }
+
+        // Check for circular dependencies
+        if (_resolutionStack.Value != null && !_resolutionStack.Value.Add(serviceType))
+        {
+            throw new InvalidOperationException(
+                $"Circular dependency detected while resolving {serviceType}. " +
+                $"Resolution chain: {string.Join(" -> ", _resolutionStack.Value.Select(t => t.Name))}");
+        }
+
+        try
+        {
+            // Try resolver overrides first
+            foreach (var resolverOverride in _overrides)
+            {
+                if (resolverOverride.TryResolve(serviceType, requestingType, this, out var overrideResult))
+                {
+                    return overrideResult;
+                }
+            }
+
+            // Handle IEnumerable<T> requests
+            if (serviceType.IsGenericType && serviceType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+            {
+                var elementType = serviceType.GetGenericArguments()[0];
+                return ResolveEnumerable(elementType);
+            }
+
+            // Try to resolve from local registrations (most recent wins)
+            if (_registrations.TryGetValue(serviceType, out var registrations) && registrations.Count > 0)
+            {
+                var registration = registrations[^1]; // Most recent registration
+                return registration.Instance ?? CreateInstance(registration.ImplementationType, serviceType);
+            }
+
+            // Delegate to parent if available
+            return _parent != null 
+                ? _parent.Resolve(serviceType) 
+                : throw new InvalidOperationException($"Service of type {serviceType} is not registered.");
+        }
+        finally
+        {
+            _resolutionStack.Value?.Remove(serviceType);
+        }
+    }
+
+    private Array ResolveEnumerable(Type elementType)
+    {
+        var instances = new List<object>();
+
+        // Collect from parent first
+        if (_parent != null)
+        {
+            try
+            {
+                var parentEnumerable = _parent.Resolve(typeof(IEnumerable<>).MakeGenericType(elementType));
+                if (parentEnumerable is IEnumerable enumerable)
+                {
+                    instances.AddRange(enumerable.Cast<object>());
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                // Parent doesn't have any, continue
+            }
+        }
+
+        // Add local registrations
+        foreach (var (type, registrations) in _registrations)
+        {
+            if (elementType.IsAssignableFrom(type))
+            {
+                foreach (var registration in registrations)
+                {
+                    instances.Add(
+                        registration.Instance ?? CreateInstance(registration.ImplementationType, type));
+                }
+            }
+        }
+
+        var array = Array.CreateInstance(elementType, instances.Count);
+        for (var i = 0; i < instances.Count; i++)
+        {
+            array.SetValue(instances[i], i);
+        }
+
+        return array;
+    }
+
+    public IResolutionScope CreateChildScope()
+    {
+        if (_disposed)
+        {
+            throw new ObjectDisposedException(nameof(ScopedContainer));
+        }
+
+        return new ScopedContainer(this);
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        _disposed = true;
+
+        foreach (var disposable in _disposables)
+        {
+            if (disposable is IDisposable d)
+            {
+                d.Dispose();
+            }
+        }
+
+        _disposables.Clear();
+        _resolutionStack.Dispose();
+    }
+
+    private object CreateInstance(Type implementationType, Type requestingType)
+    {
+        var constructors = implementationType.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+
+        if (constructors.Length == 0)
+        {
+            throw new InvalidOperationException(
+                $"Type {implementationType} has no public constructors.");
+        }
+
+        var constructor = constructors.OrderByDescending(c => c.GetParameters().Length).First();
+        var parameters = constructor.GetParameters();
+        var parameterInstances = new object[parameters.Length];
+
+        for (var i = 0; i < parameters.Length; i++)
+        {
+            parameterInstances[i] = Resolve(parameters[i].ParameterType, requestingType);
+        }
+
+        var instance = Activator.CreateInstance(implementationType, parameterInstances);
+
+        if (instance is null)
+        {
+            throw new InvalidOperationException(
+                $"Failed to create instance of type {implementationType}.");
+        }
+
+        if (instance is IDisposable)
+        {
+            _disposables.Add(instance);
+        }
+        
+        return instance;
+    }
+
+    private class Registration
+    {
+        public object? Instance { get; }
+        public Type ImplementationType { get; }
+
+        public Registration(object instance)
+        {
+            Instance = instance;
+            ImplementationType = null!;
+        }
+
+        public Registration(Type implementationType)
+        {
+            Instance = null;
+            ImplementationType = implementationType;
+        }
+    }
+}
