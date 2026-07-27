1c00a68495a7b47adb1f0945c0ef4ba838c325aa
Richard Irons <richard.irons@neo4j.com>
2026-07-27 13:32:44 +0100

    Add IMessageSerializer over JsonSerializer + IJsonOptionsProvider
    Thin injectable seam for the static JsonSerializer calls: Deserialize(json) ->
    IProtocolMessage and Serialize(message) -> string, both using the options from
    IJsonOptionsProvider. Serializing as IProtocolMessage is what engages the
    envelope converter. Keeps JsonSerializer and options out of every consumer.

---
+++ Neo4j.Driver/Neo4j.Driver.TestKitBackend.Tests/MessageSerializerTests.cs
@@ -0,0 +1,54 @@
+// Copyright (c) "Neo4j"
+// Neo4j Sweden AB [https://neo4j.com]
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using FluentAssertions;
+using Moq;
+using Neo4j.Driver.TestKitBackend.Protocol;
+using Xunit;
+
+namespace Neo4j.Driver.TestKitBackend.Tests;
+
+public class MessageSerializerTests
+{
+    private readonly Mock<IMessageTypeMap> _messageTypeMap = new();
+
+    private IMessageSerializer Subject()
+    {
+        _messageTypeMap.Setup(m => m.GetTypeByName("SampleMessage")).Returns(typeof(SampleMessage));
+        var optionsProvider = new JsonOptionsProvider([new EnvelopeConverter(_messageTypeMap.Object)]);
+        return new MessageSerializer(optionsProvider);
+    }
+
+    [Fact]
+    public void Serialize_wraps_the_message_in_its_envelope()
+    {
+        var json = Subject().Serialize(new SampleMessage { Value = "x" });
+
+        json.Should().Be("""{"name":"SampleMessage","data":{"value":"x"}}""");
+    }
+
+    [Fact]
+    public void Deserialize_reads_an_envelope_into_its_message_type()
+    {
+        var message = Subject().Deserialize("""{"name":"SampleMessage","data":{"value":"x"}}""");
+
+        message.Should().BeOfType<SampleMessage>().Which.Value.Should().Be("x");
+    }
+
+    private record SampleMessage : IProtocolMessage
+    {
+        public string Value { get; init; } = "";
+    }
+}
